172d3e90c29876c531528664e24a0d258a848b31
Adammantium <adam@devforce.de>
2022-07-13 23:07:34 +0200

    Cleanup usings

--- Program.cs
+++ Program.cs
@@ -1,11 +1,6 @@
 ﻿using AMP;
 using AMP.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace AMP_Server {
     internal class Program {
