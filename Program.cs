7c46466ef633d7db6db898ce46f679c81bee468e
Adammantium <adam@devforce.de>
2022-07-13 22:51:24 +0200

    big bang

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using AMP;
+using AMP.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AMP_Server {
+    internal class Program {
+        static void Main(string[] args) {
+            Log.loggerType = Log.LoggerType.CONSOLE;
+
+            ModManager.HostServer(10, 26950, false);
+
+            while(ModManager.serverInstance != null) {
+                Thread.Sleep(1000);
+            }
+
+        }
+    }
+}
