a7303bef29949d0549aba25ecaee8f96f7c9f29c
Adammantium <adam@devforce.de>
2022-08-28 19:03:29 +0200

    Some small changes

--- Program.cs
+++ Program.cs
@@ -1,16 +1,26 @@
 ﻿using AMP;
 using AMP.Logging;
+using AMP.Threading;
+using System;
+using System.Collections.Generic;
 using System.Threading;
+using UnityEngine;
 
 namespace AMP_Server {
     internal class Program {
         static void Main(string[] args) {
             Log.loggerType = Log.LoggerType.CONSOLE;
 
-            ModManager.HostServer(10, 26950, false);
+            Dispatcher umtd = new Dispatcher();
+            Dispatcher.current = umtd;
+
+            ModManager.HostServer(10, 26950);
+
+            ModManager.discordNetworking = false;
 
             while(ModManager.serverInstance != null) {
-                Thread.Sleep(1000);
+                Thread.Sleep(1);
+                umtd.ServerUpdateTick();
             }
 
         }
