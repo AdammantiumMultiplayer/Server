1cc44812f394c9e2b66a0454e500569fd062d8d8
Adammantium <adam@devforce.de>
2022-09-04 17:13:32 +0200

    Server working again

--- Program.cs
+++ Program.cs
@@ -1,28 +1,18 @@
 ﻿using AMP;
+using AMP.Data;
 using AMP.Logging;
 using AMP.Threading;
 using System;
-using System.Collections.Generic;
-using System.Threading;
-using UnityEngine;
 
 namespace AMP_Server {
     internal class Program {
         static void Main(string[] args) {
             Log.loggerType = Log.LoggerType.CONSOLE;
 
-            Dispatcher umtd = new Dispatcher();
-            Dispatcher.current = umtd;
+            ServerConfig.path = "server.ini";
+            ServerConfig.Load();
 
-            ModManager.HostServer(10, 26950);
-
-            ModManager.discordNetworking = false;
-
-            while(ModManager.serverInstance != null) {
-                Thread.Sleep(1);
-                umtd.ServerUpdateTick();
-            }
-
+            ModManager.HostDedicatedServer((uint) ServerConfig.maxPlayers, 26950);
         }
     }
 }
