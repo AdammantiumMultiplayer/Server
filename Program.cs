c7dba3340510631e7c17068702e158906ff23bbe
Adammantium <adam@devforce.de>
2022-09-07 23:49:27 +0200

    I like ASCII Art

--- Program.cs
+++ Program.cs
@@ -1,16 +1,61 @@
 ﻿using AMP;
 using AMP.Data;
 using AMP.Logging;
-using AMP.Threading;
 using System;
+using System.Reflection;
 
 namespace AMP_Server {
     internal class Program {
+        public static string SERVER_DEV_STATE = "Alpha";
+        public static string SERVER_VERSION = "";
+        public static string SERVER_NAME = "";
+
+        public static void ReadVersion() {
+            try {
+                string version = Assembly.GetExecutingAssembly().GetName().Version.ToString().TrimEnd(new char[] { '.', '0' });
+                if(version.Split('.').Length == 1) {
+                    version += ".0";
+                }
+                if(version.Split('.').Length == 2) {
+                    version += ".0";
+                }
+
+                SERVER_VERSION = SERVER_DEV_STATE + " " + version;
+            } catch(Exception) { // With other languages the first one seems to screw up
+                SERVER_VERSION = SERVER_DEV_STATE + " [VERSION ERROR] ";
+            }
+            SERVER_NAME = "AMP " + SERVER_VERSION;
+        }
+
+
         static void Main(string[] args) {
+            ReadVersion();
+            ModManager.ReadVersion();
+
             Log.loggerType = Log.LoggerType.CONSOLE;
 
-            ServerConfig.path = "server.ini";
-            ServerConfig.Load();
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Log.Info("");
+            Log.Info("");
+            Log.Info("<color=#FF8C00>" +
+                      ",_._._._._._._._._|__________________________________________________________.\r\n" +
+                      "|_X_X_X_X_X_X_X_X_|█████████████████████████████████████████████████████████▛\r\n" +
+                      "                  !\r\n" +
+                      "\t\t\t   █████╗ ███╗   ███╗██████╗  \r\n" +
+                      "\t\t\t  ██╔══██╗████╗ ████║██╔══██╗ \r\n" +
+                      "\t\t\t  ███████║██╔████╔██║██████╔╝ \r\n" +
+                      "\t\t\t  ██╔══██║██║╚██╔╝██║██╔═══╝  \r\n" +
+                      "\t\t\t  ██║  ██║██║ ╚═╝ ██║██║      \r\n" +
+                      "\t\t\t  ╚═╝  ╚═╝╚═╝     ╚═╝╚═╝      \r\n" +
+                     $"\t\t\t Server Version: {SERVER_VERSION}\r\n" +
+                     $"\t\t\t    Mod Version: {ModManager.MOD_VERSION}\r\n" +
+                      ".__________________________________________________________|_._._._._._._._._,\r\n" +
+                      " ▜█████████████████████████████████████████████████████████|_X_X_X_X_X_X_X_X_|\r\n" +
+                      "                                                           !\r\n" +
+                      "</color>");
+
+            ServerConfig.Load("server.ini");
 
             ModManager.HostDedicatedServer((uint) ServerConfig.maxPlayers, 26950);
         }
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Adammantium Multiplayer Mod Standalone")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("AMP Server")]
+[assembly: AssemblyDescription("Dedicated Server Wrapper for AMP")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Adammantium Multiplayer Mod Standalone")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCompany("Adammantium @ DevForce.de")]
+[assembly: AssemblyProduct("AMP Server")]
+[assembly: AssemblyCopyright("Copyright © 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
