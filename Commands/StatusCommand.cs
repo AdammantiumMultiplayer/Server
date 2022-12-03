4b5019b3717fc9141229e14289966c5ae6ccd52d
Adammantium <adam@devforce.de>
2022-12-03 01:26:04 +0100

    Added map to status command

--- Commands/StatusCommand.cs
+++ Commands/StatusCommand.cs
@@ -10,6 +10,7 @@ namespace AMP.DedicatedServer.Commands {
 
         public override string Process(string[] args) {
             Log.Info("Status:");
+            Log.Info("        Map: " + ModManager.serverInstance.currentLevel + " / " + ModManager.serverInstance.currentMode);
             Log.Info("      Items: " + ModManager.serverInstance.spawnedItems);
             Log.Info("  Creatures: " + ModManager.serverInstance.spawnedCreatures);
             Log.Info("    Clients: " + ModManager.serverInstance.connectedClients + " / " + ModManager.serverInstance.maxClients);
