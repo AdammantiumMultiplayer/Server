b9db8c4805bcacbfcac8bb8a45e0e815db0b3a58
Adammantium <adam@devforce.de>
2023-08-29 23:08:35 +0200

    Allow for config to be saved by the plugin creator

--- Plugins/PluginConfig.cs
+++ Plugins/PluginConfig.cs
@@ -1,5 +1,9 @@
 ﻿namespace AMP.DedicatedServer.Plugins {
     public class PluginConfig {
 
+
+        public static void Save(AMP_Plugin plugin) {
+            PluginConfigLoader.SaveConfig(plugin);
+        }
     }
 }
