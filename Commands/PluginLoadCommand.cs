f7196f7daa2d207ec5d50b46582c41b6d47648d2
RaphaelIT7 <raphael.i.t@outlook.de>
2024-02-14 21:46:43 +0100

    A few more changes
    [+] Added pl_load and pl_unload to load and unload plugins without restarting.
    [+] Added Plugin Autorefresh
    - [+] Added a Config option to enable it.
    - [#] Using Assembly.Load instead of Assembly.LoadFrom to allow one to change the plugin while it's loaded (for Autorefresh)
    Reloads a Plugin if it was changed.
    [#] Configs are now saved with the file as the key.
    Solves an Issue where if a plugin is loaded multiple times, it would have a config for each plugin instance instead of just one.
    [#] Fixed a bug where plugins were not removed from the loadedPlugins list.
    [#] LoadPlugin now returns a PLUGIN_STATUS (OK, ERROR, NOFILE)
    Also made the function public
    [#] Configs are now unloaded when a Plugin is unloaded.

---
+++ Commands/PluginLoadCommand.cs
@@ -0,0 +1,37 @@
+﻿using AMP.DedicatedServer.Data;
+using AMP.Logging;
+using System.IO;
+using static AMP.DedicatedServer.PluginLoader;
+
+namespace AMP.DedicatedServer.Commands {
+    internal class PluginLoadCommand : CommandHandler {
+
+        public override string[] ALIASES => new string[] { "pl_load" };
+        public override string   HELP    => "Loads the plugin with the given name.";
+
+        public override string Process(string[] args) {
+            if (args.Length == 0) {
+                return $"Usage: pl_load [file name]";;
+            }
+
+            AMP_Plugin plugin = PluginLoader.GetPlugin(Path.GetFullPath("plugins/" + (args[0].EndsWith(".dll") ? args[0] : (args[0] + ".dll"))));
+            if (plugin != null) {
+                return $"Plugin \"{ plugin.NAME }\" is already loaded.";
+            }
+
+            PLUGIN_STATUS loaded = PluginLoader.LoadPlugin("plugins/" + (args[0].EndsWith(".dll") ? args[0] : (args[0] + ".dll")));
+
+            switch (loaded) {
+                case PLUGIN_STATUS.OK:
+                    PluginWatcher.AddFile(plugin.FILE);
+                    return $"Plugin \"{ args[0] }\" was loaded.";
+                case PLUGIN_STATUS.ERROR:
+                    return $"Plugin \"{ args[0] }\" had an error while loading.";
+                case PLUGIN_STATUS.NOFILE:
+                    return $"Plugin \"{ args[0] }\" was not found.";
+                default:
+                    return null;
+            }
+        }
+    }
+}
---
+++ Commands/PluginUnloadCommand.cs
@@ -0,0 +1,22 @@
+﻿using AMP.DedicatedServer.Data;
+using AMP.Logging;
+using System.IO;
+
+namespace AMP.DedicatedServer.Commands {
+    internal class PluginUnloadCommand : CommandHandler {
+
+        public override string[] ALIASES => new string[] { "pl_unload" };
+        public override string   HELP    => "Unloads the plugin with the given name.";
+
+        public override string Process(string[] args) {
+            if (args.Length == 0) {
+                return $"Usage: pl_unload [plugin name]";;
+            }
+
+            bool success = PluginLoader.UnloadPlugin(args[0]);
+            PluginWatcher.RemoveFile(Path.GetFullPath("plugins/" + (args[0].EndsWith(".dll") ? args[0] : (args[0] + ".dll"))));
+
+            return success ? $"Plugin \"{ args[0] }\" was unloaded." : $"Plugin \"{ args[0] }\" was not found or loaded?";
+        }
+    }
+}
